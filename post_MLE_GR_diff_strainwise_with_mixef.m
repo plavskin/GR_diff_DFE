c5b3888fedcc92c2bd4d258b48b43d4c458fd633
plavskin <eugene.plavskin@gmail.com>
2019-04-08 17:39:17 -0400

    Split post_MLE into two different modes: post_MLE_GR_diff_strainwise_with_mixef, which includes "mutational effect" parameters for estimation of strainwise mutational effects, and post_MLE_GR_diff_strainwise_DFE_with_mixef.m, which instead includes "distribution of fitness effects (DFE)" parameters and estimates a DFE from which individual strain mutational effects are drawn.

--- post_MLE_GR_diff_strainwise_with_mixef.m
+++ post_MLE_GR_diff_strainwise_with_mixef.m
@@ -2,20 +2,20 @@ function output_dict = post_MLE_GR_diff_strainwise_with_mixef(input_value_dict, 
 
     output_file = input_value_dict('output_file');
     global_output_table = readtable(output_file);
+    test_strain_ML_file = pre_MLE_output_dict('test_strain_ML_file');
+    test_strain_ML_table = readtable(test_strain_ML_file);
 
-    lower_level_param_ML_file = pre_MLE_output_dict('test_strain_ML_file');
-    lower_level_param_ML_table = readtable(lower_level_param_ML_file);
-    lower_level_param_ML_table_names = ...
-        lower_level_param_ML_table.Properties.VariableNames;
+    strain_names = test_strain_ML_table.Strain_Names';
+    me_mle_vals = test_strain_ML_table.Mutation_Effects;
+    pp_mle_vals = test_strain_ML_table.Petite_Proportions;
 
-    % retrieve DFE parameter info
-    DFE_parameters = input_value_dict('DFE_parameters');
+    strain_param_name_list = [strcat(strain_names,'_me'), ...
+        strcat(strain_names,'_pp')];
+    strain_table_data = num2cell([me_mle_vals', pp_mle_vals']);
+    strain_table = table(strain_table_data{:},'VariableNames', strain_param_name_list);
 
-    final_table = [global_output_table lower_level_param_ML_table];
-    final_table = ...
-        movevars(final_table,'point_num', 'After', ...
-            lower_level_param_ML_table_names{end});
-    final_table = movevars(final_table, DFE_parameters, 'After', 'runtime_in_secs');
+    final_table = [global_output_table strain_table];
+    final_table = movevars(final_table,'point_num','After',strain_param_name_list{end});
 
     writetable(final_table,output_file);
 
