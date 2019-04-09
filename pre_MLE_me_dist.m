980eaabe933babdcc8f70309722a7b0402b60af9
plavskin <eugene.plavskin@gmail.com>
2019-04-09 13:42:47 -0400

    Added separate grid size for estimation of GR diff likelihood, which does not need need to be estimated on as dense a grid as the distribution of mutational effects itself

--- pre_MLE_me_dist.m
+++ pre_MLE_me_dist.m
@@ -4,6 +4,7 @@ function output_dict = pre_MLE_me_dist(input_value_dict)
     gridpower = input_value_dict('gridpower');
     smoothing_kernel_sigma = input_value_dict('smoothing_kernel_sigma');
     L = input_value_dict('L');
+    strain_mean_gridpower = input_value_dict('strain_mean_gridpower');
 
     N = 2^gridpower;
     [me_pdf_frequency_vals, me_pdf_xvals] = fourier_space_creator(N, L);
@@ -11,8 +12,11 @@ function output_dict = pre_MLE_me_dist(input_value_dict)
     F_kernel = fourier_domain_gauss(me_pdf_frequency_vals, 0, ...
         smoothing_kernel_sigma, {}, false);
     
+    [~, strain_pdf_xvals] = fourier_space_creator(2^strain_mean_gridpower, L);
+
     output_dict = ...
-        containers.Map({'N', 'me_pdf_frequency_vals', 'me_pdf_xvals', 'F_kernel'}, ...
-        {N, me_pdf_frequency_vals, me_pdf_xvals, F_kernel});
+        containers.Map({'N', 'me_pdf_frequency_vals', 'me_pdf_xvals', ...
+            'F_kernel', 'strain_pdf_xvals'}, ...
+        {N, me_pdf_frequency_vals, me_pdf_xvals, F_kernel, strain_pdf_xvals});
 
 end
